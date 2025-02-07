93cd1f8de1cb69ce161ef745123a61dc56a8e6af
Leonardo Viana Teixeira <108376948+lvteixeira@users.noreply.github.com>
2025-02-06 22:55:39 -0300

    Create useCreateUsuario.ts
    melhoria na integração react query

---
+++ src/app/hooks/useCreateUsuario.ts
@@ -0,0 +1,20 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { UsuarioService } from "../service/UsuarioService";
+import type { Usuario } from "../types/Usuario";
+import { USERS_QUERY_KEY } from "./useUsuarios";
+
+export function useCreateUsuario() {
+  const service = new UsuarioService();
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: (usuario: Usuario) => service.criarUsuario(usuario),
+    onSuccess: (newUsuario: Usuario) => {
+      //queryClient.setQueryData(USERS_QUERY_KEY, (old: Usuario[] = []) => [...old, newUsuario]);
+      queryClient.invalidateQueries({ queryKey: USERS_QUERY_KEY });
+    },
+    onError: (error: Error) => {
+      console.error("Falha ao criar usuário", error.message);
+    },
+  });
+}
